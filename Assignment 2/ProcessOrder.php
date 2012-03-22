f1d07a1004bb2adbe68e2203fc771b20873a355c
Desmond Poh <mazatsushi@gmail.com>
2012-03-22 17:14:13 +0800

    - PHP server working
    Signed-off-by: Desmond Poh <mazatsushi@gmail.com>

---
+++ Assignment 2/ProcessOrder.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Order Processed</title>
+</head>
+<body>
+	<?php
+	echo "Hello World!";
+	?>
+</body>
+</html>
