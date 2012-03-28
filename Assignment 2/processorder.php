9476984cf237e987a3a5374ed032255e08f1fe61
Desmond Poh <mazatsushi@gmail.com>
2012-03-28 16:31:04 +0800

    - Output receipt looks acceptable - Proceed on to handle the exception output
    Signed-off-by: Desmond Poh <mazatsushi@gmail.com>

--- Assignment 2/processorder.php
+++ Assignment 2/processorder.php
@@ -18,7 +18,7 @@
 		if (is_null($username)) {
 			array_push($errorMsg, "Invalid user name.");
 		}
-		if (is_null($apples) || is_nan($$apples) || $apples < 0) {
+		if (is_null($apples) || is_nan($apples) || $apples < 0) {
 			array_push($errorMsg, "Invalid number of apples.");
 		}
 		if (is_null($bananas) || is_nan($bananas) || $bananas < 0) {
@@ -31,35 +31,62 @@
 			array_push($errorMsg, "Invalid payment mode.");
 		}
 		if (sizeOf($errorMsg) > 0) {
+			$errorMsg = array_reverse($errorMsg);
 			throw new UnexpectedValueException($errorMsg);
 		}
 
+		$total = (0.69 * $apples) + (0.59 * $bananas) + (0.39 * $oranges);
+
 		echo '<div class="receipt">
-		<div class="header">
-		<label class="summary">Your order has been received.</label>
+		<div class="summary">
+		<label>Your order has been received.</label>
 		</div>
 		<div class="header">
-		<label>Username: </label> <label class="data">';
+		<label>Username: </label>
+		</div>
+		<div class="data">
+		<label>';
 		echo $username;
 		echo '</label>
 		</div>
-		<div class="header">
-		<label>Number of apples: </label> <label class="data">';
+		<div class="alt header">
+		<label>Number of apples: </label>
+		</div>
+		<div class="alt data">
+		<label>';
 		echo $apples;
 		echo '</label>
 		</div>
 		<div class="header">
-		<label>Number of bananas: </label> <label class="data">';
+		<label>Number of bananas: </label>
+		</div>
+		<div class="data">
+		<label>';
 		echo $bananas;
 		echo '</label>
 		</div>
-		<div class="header">
-		<label>Number of oranges: </label> <label class="data">';
+		<div class="alt header">
+		<label>Number of oranges: </label>
+		</div>
+		<div class="alt data">
+		<label>';
 		echo $oranges;
 		echo '</label>
 		</div>
 		<div class="header">
-		<label>Total Cost: $</label>
+		<label>Total Cost: $</label></div>
+		<div class="data">
+		<label>';
+		echo $total;
+		echo '</label>
+		</div>
+		<div class="alt header">
+		<label>Paid Using: </label>
+		</div>
+		<div class="alt data">
+		<label>';
+		echo $payment;
+		echo '</label>
 		</div>
 		</div>';
 	}
